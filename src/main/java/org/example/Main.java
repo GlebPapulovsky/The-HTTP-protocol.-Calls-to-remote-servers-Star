98f2b4637ce70a865b14ccc4e8706acfcf142c73
GlebPapulovsky <papulovskijglech@gmail.com>
2024-09-24 21:23:54 +0300

    add saving jpg

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,62 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Main {
+    //usr + key for NASA server
+    public static final String REMOTE_SERVER_URL = "https://api.nasa.gov/planetary/apod?api_key=6MMbh7SpNPsk4rTYgq6xJoeYPlYp2w5ZjcImDPag";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("Gleb Papulovsky")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(10000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+        HttpGet request = new HttpGet(REMOTE_SERVER_URL);
+        //request.setHeader(HttpHeaders.ACCEPT);
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        //Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        String body = new String(response.getEntity().getContent().readAllBytes());
+        String photoUrl = "";
+        String[] strs = body.split("\"");
+        for (String str : strs) {
+            if (str.contains(".jpg")) {
+                photoUrl = str;
+                break;
+            }
         }
+        System.out.println("Photos url: " + photoUrl);
+//        HttpGet httpRequest=new HttpGet(photoUrl);
+//        CloseableHttpResponse httpResponse= httpClient.execute( httpRequest);
+//        System.out.println(Arrays.toString(httpResponse.getEntity().getContent().readAllBytes()));
+        try (InputStream in = new URL(photoUrl).openStream()) {
+            Files.copy(in, Paths.get("G://image.jpg"));
+        }
+
+
     }
+
 }
